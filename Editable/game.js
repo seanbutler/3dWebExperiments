197dae72cafc6399947c6cf24141540ca8ba9626
seanbutler <butlersean@gmail.com>
2019-04-22 17:05:27 +0100

    ok, this is a gooer

--- Editable/game.js
+++ Editable/game.js
@@ -14,14 +14,39 @@ context.scale(dpr, dpr);
 
 // ---------------------------------------------------------------------------
 
+function Pulse() {
 
+    this.init = function() {
+        this.x = Math.random()*width;
+        this.y = Math.random()*height;
+        this.color = "rgb(" + Math.random()*128 + ", " + Math.random()*128 + ", " + Math.random()*128 + ")";
+        this.size = width/4 +  Math.random()*width/4;
+    }
 
+    this.update = function () {
+        this.size *= 0.99;
+    }
 
+    this.draw = function () {
+        context.beginPath();
+        context.fillStyle = this.color;
+        // context.rect(this.x - this.size/2, this.y - this.size/2, this.size, this.size);
+        context.ellipse(this.x,
+                        this.y,
+                        this.size,
+                        this.size,
+                        0,
+                        8,
+                        1);
+        context.fill();
+    }
+
+    this.init();
+}
 
 // ---------------------------------------------------------------------------
 
-
-function Ball() {
+function Boing() {
 
     this.init = function() {
         this.x = width / 2;
@@ -30,7 +55,7 @@ function Ball() {
         this.dx = (this.speed * Math.random()) - ( this.speed * 0.5 );
         this.dy = (this.speed * Math.random()) - ( this.speed * 0.5 );
         this.color = "rgb(" + 128+Math.random()*128 + ", " + Math.random()*128 + ", " + Math.random()*128 + ")";
-        this.size = width/32 + Math.random()*width/32;
+        this.size = width/32 +  Math.random()*width/32;
     }
 
     this.update = function () {
